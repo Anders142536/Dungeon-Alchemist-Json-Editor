fafffce7f534f1e96ae453d0832d02fe6ca0c378
Anders <dom.galbavy@gmail.com>
2022-07-31 01:06:46 +0200

    added .prettierrc and setup formatter

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import Inspector from './inspector/Inspector.svelte'
-import Map from './map/Map.svelte';
-	import Topbar from './topbar/Topbar.svelte'
-	
-
+  import Inspector from './inspector/Inspector.svelte'
+  import Map from './map/Map.svelte'
+  import Topbar from './topbar/Topbar.svelte'
 </script>
 
-<Map/>
+<Map />
 <Topbar />
 <Inspector />
--- src/inspector/Inspector.svelte
+++ src/inspector/Inspector.svelte
@@ -1,16 +1,15 @@
-<script>
-	import LightsEditor from './LightsEditor.svelte'
-	import WallsEditor	from './WallsEditor.svelte'
+<script lang="ts">
+  import LightsEditor from './LightsEditor.svelte'
+  import WallsEditor from './WallsEditor.svelte'
 
-
+  let test: number = 3
 </script>
 
-
-<div id='inspector' class='interface'>
-	<LightsEditor/>
-	<WallsEditor/>
-	<div class='button-bar'>
-		<input type='button' value='Cancel'>
-		<input type='button' value='Save'>
-	</div>
+<div id="inspector" class="interface">
+  <LightsEditor />
+  <WallsEditor />
+  <div class="button-bar">
+    <input type="button" value="Cancel" />
+    <input type="button" value="Save" />
+  </div>
 </div>
